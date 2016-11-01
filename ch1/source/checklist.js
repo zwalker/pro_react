5bb375a7545ae73929c3885a07a3084a83bbb2cb
Zach Walker <zach.walker@appfolio.com>
2016-11-01 12:44:07 -0700

    zw - chapter 1, part 1 kanban board without state

---
+++ ch1/source/checklist.js
@@ -0,0 +1,23 @@
+import React from 'react';
+
+class CheckList extends React.Component {
+  render() {
+    console.log(this.props.tasks);
+    let tasks = this.props.tasks.map((task) => (
+      <li className='checklist__task'>
+        <input type='checkbox' defaultChecked={task.done} />
+        {task.name}
+        <a href='#' className='checklist__task--remove' />
+      </li>
+    ));
+
+    console.log(tasks);
+    return (
+      <div className='checklist'>
+        <ul>{tasks}</ul>
+      </div>
+    );
+  }
+}
+
+export default CheckList;
