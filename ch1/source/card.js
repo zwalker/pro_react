ef9d2602decbe5265c9bd814b53a80efa2b23426
Zach Walker <zach.walker@appfolio.com>
2016-11-06 21:24:10 -0800

    zw - toggle visibility of card details using manages state

--- ch1/source/card.js
+++ ch1/source/card.js
@@ -2,14 +2,30 @@ import React from 'react';
 import CheckList from './checklist.js';
 
 class Card extends React.Component {
+  constructor() {
+    super(...arguments);
+    this.state = {
+      showDetails: false
+    };
+  }
+
   render() {
-    return (
-      <div className='card'>
-        <div className='card__title'>{this.props.title}</div>
+    let cardDetails;
+    if(this.state.showDetails) {
+      cardDetails = (
         <div className='card__details'>
           {this.props.description}
           <CheckList card_id={this.props.id} tasks={this.props.tasks} />
         </div>
+      )
+    }
+
+    return (
+      <div className='card'>
+        <div className='card__title' onClick={
+          ()=>this.setState({showDetails: !this.state.showDetails})
+        }>{this.props.title}</div>
+        {cardDetails}
       </div>
     );
   }
