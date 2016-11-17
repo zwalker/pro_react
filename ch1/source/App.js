9fa2261dfac567a24080f809574c5da637b22feb
Zach Walker <zach.walker@appfolio.com>
2016-11-16 21:24:29 -0800

    zw - add a component for fetching data on application load

--- ch1/source/App.js
+++ ch1/source/App.js
@@ -1,40 +1,42 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-import KanbanBoard from './KanbanBoard';
+import KanbanBoardContainer from './kanban_board_container';
 
-let cardsList = [
-  {
-    id: 1,
-    title: "Read the Book",
-    description: "I should read the **whole** book",
-    color: '#BD8D31',
-    status: "in-progress",
-    tasks: []
-  },
-  {
-    id: 2,
-    title: "Write some code",
-    description: "Code along with the samples in the book.  The complete source can be found at [github](https://github.com/pro-react)",
-    color: '#3A7E28',
-    status: "todo",
-    tasks: [
-      {
-        id: 1,
-        name: "ContactList Example",
-        done: true
-      },
-      {
-        id: 2,
-        name: "Kanban Example",
-        done: false
-      },
-      {
-        id: 3,
-        name: "My own experiments",
-        done: false
-      }
-    ]
-  }
-];
+//let cardsList = [
+//  {
+//    id: 1,
+//    title: "Read the Book",
+//    description: "I should read the **whole** book",
+//    color: '#BD8D31',
+//    status: "in-progress",
+//    tasks: []
+//  },
+//  {
+//    id: 2,
+//    title: "Write some code",
+//    description: "Code along with the samples in the book.  The complete source can be found at [github](https://github.com/pro-react)",
+//    color: '#3A7E28',
+//    status: "todo",
+//    tasks: [
+//      {
+//        id: 1,
+//        name: "ContactList Example",
+//        done: true
+//      },
+//      {
+//        id: 2,
+//        name: "Kanban Example",
+//        done: false
+//      },
+//      {
+//        id: 3,
+//        name: "My own experiments",
+//        done: false
+//      }
+//    ]
+//  }
+//];
+//
+// ReactDom.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
 
-ReactDom.render(<KanbanBoard cards={cardsList} />, document.getElementById('root'));
+ReactDom.render(<KanbanBoardContainer />, document.getElementById('root'));
---
+++ ch1/source/kanban_board_container.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import KanbanBoard from './KanbanBoard';
+import 'whatwg-fetch';
+
+const API_URL = 'http://kanbanapi.pro-react.com';
+const API_HEADERS = {
+  'Content-Type': 'application/json',
+  Authorization: 'mewford'
+};
+
+class KanbanBoardContainer extends Component {
+  constructor(){
+    super(...arguments);
+    this.state = {
+      cards: []
+    };
+  }
+  
+  componentDidMount(){
+    fetch(API_URL+'/cards', {headers: API_HEADERS})
+    .then((response) => response.json())
+    .then((responseData) => {
+      this.setState({cards: responseData});
+    })
+    .catch((error) => {
+      console.log('Error fetching and parsing data', error);
+    });
+  } 
+
+  render() {
+    return <KanbanBoard cards={this.state.cards} />
+  }
+}
+
+export default KanbanBoardContainer;
