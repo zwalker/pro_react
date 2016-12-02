df952d3867acae01834e987ddf3cab78a31d30f4
Zach Walker <zach.walker@appfolio.com>
2016-12-01 21:10:55 -0800

    zw - initial project stub

---
+++ github_route_example/webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  devtool: 'eval-source-map',
+  entry: [
+    './source/app.js',
+  ],
+  output: {
+    path: __dirname,
+    filename: "bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      loader: 'babel',
+      query: {
+        presets: ['es2015', 'react']
+      }
+    }]
+  },
+};
