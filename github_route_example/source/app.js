8342610521eaf3a3befb9744ee5d22d7676d1508
Zach Walker <zach.walker@appfolio.com>
2016-12-01 22:12:10 -0800

    zw - use react-router module to do routing

--- github_route_example/source/app.js
+++ github_route_example/source/app.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 
+import { Router, Route, IndexRoute, Link } from 'react-router';
+
 import About from './about';
 import Home from './home';
 import Repos from './repos';
@@ -22,26 +24,28 @@ class App extends Component {
   }
 
   render() {
-    var Child;
-    switch (this.state.route) {
-      case '/about': Child = About; break;
-      case '/repos': Child = Repos; break;
-      default:       Child = Home;
-    }
-
     return (
       <div>
         <header>App</header>
         <menu>
           <ul>
-            <li><a href='#/about'>About</a></li>
-            <li><a href='#/repos'>Repos</a></li>
+            <li><Link to='/about'>About</Link></li>
+            <li><Link to='/repos'>Repos</Link></li>
           </ul>
         </menu>
-        <Child/>
+        {this.props.children}
       </div>
     )
   }
 }
 
-ReactDom.render(<App />, document.getElementById('root'));
+ReactDom.render((
+  <Router>
+    <Route path='/' component={App}>
+      <IndexRoute component={Home}/>
+      <Route path='about' component={About}/>
+      <Route path='repos' component={Repos}/>
+    </Route>
+  </Router>),
+  document.getElementById('root')
+);
