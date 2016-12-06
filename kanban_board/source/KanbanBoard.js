1a1b76938483fe551982d6cf70dfac0150ef12d5
Zach Walker <zach.walker@appfolio.com>
2016-12-05 16:56:56 -0800

    zw - add button to create a new card

--- kanban_board/source/KanbanBoard.js
+++ kanban_board/source/KanbanBoard.js
@@ -2,6 +2,7 @@ import React, { Component, PropTypes } from 'react';
 import { DragDropContext } from 'react-dnd';
 import HTML5Backend from 'react-dnd-html5-backend';
 import List from './List';
+import { Link } from 'react-router';
 
 class KanbanBoard extends Component {
   render() {
@@ -11,6 +12,7 @@ class KanbanBoard extends Component {
     });
     return (
       <div className="app">
+        <Link to='/new' className="float-button">+</Link>
         <List id='todo' title='To Do' taskCallbacks={this.props.taskCallbacks} cards={
           this.props.cards.filter((card) => card.status === "todo")} cardCallbacks={
           this.props.cardCallbacks}  
