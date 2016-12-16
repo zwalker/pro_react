685cd0f1f9c2b64fd400abfecc8389233a829acb
Zach Walker <zach.walker@appfolio.com>
2016-12-16 13:51:50 -0800

    zw - setup the app and register it's dependence on the bank account balance store
    Create a new bank account when initialized
    On mount, register to receive events from emitted by the bank balance store
    On removal, make sure to remove subscriptions to the bank account store.

--- bank_account_flux_example/app/App.js
+++ bank_account_flux_example/app/App.js
@@ -1,7 +1,31 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
+import BankBalanceStore from './bank_balance_store';
+import BankActions from './bank_actions';
 
 class App extends Component {
+  constructor() {
+    super(...arguments);
+    BankActions.createAccount();
+    this.state = {
+      balance: BankBalanceStore.getState()
+    }
+  }
+
+  componentDidMount() {
+    this.storeSubscription = BankBalanceStore.addListener((data) => {
+      this.handleStoreChange(data);
+    });
+  }
+
+  componentWillUnmount() {
+    this.storeSubscription.remove();
+  }
+
+  handleStoreChange() {
+    this.setState({balance: BankBalanceStore.getState()});
+  }
+
   render(){
     return (
       <h1>Hello World</h1>
