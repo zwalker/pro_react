ac033c76ee86fb86728e8011d2c535676d2d62cc
Zach Walker <zach.walker@appfolio.com>
2016-12-16 14:31:52 -0800

    zw - refactor bank account store to use extend a flux Store

--- bank_account_flux_example/app/bank_balance_store.js
+++ bank_account_flux_example/app/bank_balance_store.js
@@ -1,38 +1,32 @@
-import { EventEmitter } from 'fbemitter';
 import appDispatcher from './app_dispatcher';
+import { Store } from 'flux/utils';
 import bankConstants from './bank_constants';
 
-const CHANGE_EVENT = 'change';
-let __emitter = new EventEmitter();
-let balance = 0;
+let __balance = 0;
 
-let BankBalanceStore = {
+class BankBalanceStore extends Store {
 
   getState() {
-    return balance;
-  },
-
-  addListener(callback) {
-    return __emitter.addListener(CHANGE_EVENT, callback);
+    return __balance;
   }
-};
 
-BankBalanceStore.dispatchToken = appDispatcher.register((action) => {
-  switch (action.type) {
-    case bankConstants.CREATED_ACCOUNT:
-      balance = 0;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.DEPOSITED_INTO_ACCOUNT:
-      balance = balance + action.amount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
-    case bankConstants.WITHDREW_FROM_ACCOUNT:
-      balance = balance - action.amount;
-      __emitter.emit(CHANGE_EVENT);
-      break;
+  __onDispatch(action) {
+    switch (action.type) {
+      case bankConstants.CREATED_ACCOUNT:
+        __balance = 0;
+        this.__emitChange();
+        break;
+      case bankConstants.DEPOSITED_INTO_ACCOUNT:
+        __balance = __balance + action.amount;
+        this.__emitChange();
+        break;
+      case bankConstants.WITHDREW_FROM_ACCOUNT:
+        __balance = __balance - action.amount;
+        this.__emitChange();
+        break;
+    }
   }
-});
+}
 
-export default BankBalanceStore;
+export default new BankBalanceStore(appDispatcher);
 
