4f66c33d60e52d540c4b8d98344c5b1b51ba7269
Zach Walker <zach.walker@appfolio.com>
2016-12-16 14:09:48 -0800

    zw - render balance, input and buttons used to change balance
    Trigger bank acctions for withdraw and depoit on button clicks

--- bank_account_flux_example/app/App.js
+++ bank_account_flux_example/app/App.js
@@ -26,9 +26,28 @@ class App extends Component {
     this.setState({balance: BankBalanceStore.getState()});
   }
 
-  render(){
+  deposit() {
+    BankActions.depositIntoAccount(Number(this.refs.amount.value));
+    this.refs.amount.value = '';
+  }
+
+  withdraw() {
+    BankActions.withdrawFromAccount(Number(this.refs.amount.value));
+    this.refs.amount.value = '';
+  }
+
+  render() {
     return (
-      <h1>Hello World</h1>
+      <div>
+        <header>Bank of Zach</header>
+        <h1>Your balance is ${(this.state.balance).toFixed(2)}</h1>
+        <div className='atm'>
+          <input type='text' placeholder='Enter Amount' ref='amount' />
+          <br />
+          <button onClick={this.withdraw.bind(this)}>Withdraw</button>
+          <button onClick={this.deposit.bind(this)}>Deposit</button>
+        </div>
+      </div>
     );
   }
 }
