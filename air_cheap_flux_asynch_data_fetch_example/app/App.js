b0ca2936d2a41edffed9b209b7e2d169333deed9
Zach Walker <zach.walker@appfolio.com>
2016-12-30 12:16:36 -0800

    zw - render the basic airport lookup on the index page making use of the autosuggest widget to make suggestions for airports based on user entered text

--- air_cheap_flux_asynch_data_fetch_example/app/App.js
+++ air_cheap_flux_asynch_data_fetch_example/app/App.js
@@ -1,9 +1,24 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
+import { Container } from 'flux/utils';
+import Autosuggest from 'react-autosuggest-legacy';
 import AirportStore from './stores/airport_store';
 import AirportActionCreators from './actions/airport_action_creators';
 
 class App extends Component {
+
+  getSuggestions(input, callback) {
+    const escapedInput = input.trim().toLowerCase();
+    const airportMatchRegex = new RegExp('\\b' + escapedInput, 'i');
+    const suggestions = this.state.airports
+      .filter(airport => airportMatchRegex.test(airport.city))
+      .sort((airport1, airport2) => {
+        return airport1.city.toLowerCase().indexOf(escapedInput) - airport2.city.toLowerCase().indexOf(escapedInput)
+      })
+      .slice(0, 7)
+      .map(airport => `${airport.city} - ${airport.country} (${airport.code})`);
+    callback(null, suggestions);
+  }
 
   componentDidMount() {
     AirportActionCreators.fetchAirports();
@@ -11,10 +26,31 @@ class App extends Component {
 
   render(){
     return (
-      <h1>Hello World</h1>
+      <div>
+        <header>
+          <div className='header-brand'>
+            <img src='logo.png' height='35'/>
+            <p>Check discount ticket prices an pay using AirCheap points</p>
+          </div>
+          <div className='header-route'>
+            <Autosuggest id='origin'
+                         suggestions={this.getSuggestions.bind(this)}
+                         inputAttributes={{placeholder: 'From'}} />
+            <Autosuggest id='destination'
+                         suggestions={this.getSuggestions.bind(this)}
+                         inputAttributes={{placeholder: 'To'}} />
+          </div>
+        </header>
+      </div>
     );
   }
 }
 
 App.getStores = () => ([AirportStore]);
-render(<App />, document.getElementById('root'));
+App.calculateState = (prevState) => ({
+  airports: AirportStore.getState()
+});
+
+const AppContainer = Container.create(App);
+render(<AppContainer />, document.getElementById('root'));
+
