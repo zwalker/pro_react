8cdff9daa632ca3aa91073cde374d1978468aea1
Zach Walker <zach.walker@appfolio.com>
2016-12-30 12:14:29 -0800

    zw - trigger airport lookup on app component load and put the data in the airport store

--- air_cheap_flux_asynch_data_fetch_example/app/App.js
+++ air_cheap_flux_asynch_data_fetch_example/app/App.js
@@ -1,7 +1,14 @@
 import React, { Component } from 'react';
 import {render} from 'react-dom';
+import AirportStore from './stores/airport_store';
+import AirportActionCreators from './actions/airport_action_creators';
 
 class App extends Component {
+
+  componentDidMount() {
+    AirportActionCreators.fetchAirports();
+  }
+
   render(){
     return (
       <h1>Hello World</h1>
@@ -9,4 +16,5 @@ class App extends Component {
   }
 }
 
+App.getStores = () => ([AirportStore]);
 render(<App />, document.getElementById('root'));
---
+++ air_cheap_flux_asynch_data_fetch_example/app/stores/airport_store.js
@@ -0,0 +1,21 @@
+import appDispatcher from '../app_dispatcher';
+import constants from '../constants';
+import { ReduceStore } from 'flux/utils';
+
+class AirportStore extends ReduceStore {
+
+  getInitialState() {
+    return [];
+  }
+
+  reduce(state, action) {
+    switch(action.type) {
+      case constants.FETCH_AIRPORTS_SUCCESS:
+        return action.payload.response;
+      default:
+        return state;
+    }
+  }
+}
+
+export default new AirportStore(appDispatcher);
